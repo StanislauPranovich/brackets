5accd1f19e1d0f9aea1f4238cab871822a902051
Stanislau Pranovich <stanislau.pranovich@gmail.com>
2020-10-14 18:51:08 +0300

    feat: add task solution brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let arr = [];
+    let obj = Object.fromEntries(bracketsConfig);
+    str = str.split('');
+    for(let key of str) {
+        if(arr[0] === key) {
+            arr.shift();
+        } else if(obj[key]) {
+            arr.unshift(obj[key]);
+        } else {
+            return false;
+        }
+    }
+    return arr.length === 0;
 }
